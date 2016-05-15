a686b914a7391e62e9c0aed8eebf9267fe1e4dbe
thanksdanny <hj-07@163.com>
2016-05-15 17:29:39 +0800

    Initial Commit

---
+++ NewDota2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NewDota2
+//
+//  Created by Danny Ho on 5/15/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ NewDota2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NewDota2
+//
+//  Created by Danny Ho on 5/15/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
