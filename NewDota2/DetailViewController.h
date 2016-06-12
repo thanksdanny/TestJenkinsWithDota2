11d44522c86ed36253e1aec545c7617b575c61f6
thanksdanny <hj-07@163.com>
2016-06-12 16:47:51 +0800

    test

---
+++ NewDota2/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  NewDota2
+//
+//  Created by Danny Ho on 5/17/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
---
+++ NewDota2/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  NewDota2
+//
+//  Created by Danny Ho on 5/16/16.
+//  Copyright © 2016 thanksdanny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
